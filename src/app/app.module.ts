c8716a39460d92b7f3511d074123e50ad56aba63
Mohammad Javad Zamani <s176485@win.dtu.dk>
2021-03-01 15:15:51 +0100

    no important

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PostCreateComponent} from './post/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HeaderComponent} from './header/header.component'
+import {PostListComponent} from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    PostCreateComponent,
+    AppComponent, 
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,11 @@
+export class post {
+    id?:string;
+    title?: string;
+    content?: string;
+    price?:Number
+    constructor(){
+      this.id=''
+      this.title=''
+      this.content=''
+    }
+    }
