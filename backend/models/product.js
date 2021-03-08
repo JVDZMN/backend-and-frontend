99c32a6995bb4e93d5b3f2638c7a9ac646ac09f1
Mohammad Javad Zamani <s176485@win.dtu.dk>
2021-03-08 13:15:00 +0100

    user,product,....

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,9 +1,61 @@
 const mongoose = require('mongoose')
-const postSchema=mongoose.Schema({
-
-    title:{type:String , required:true,default:'hello'},
-    description:{type:String , required:true,default:'my content is: '},
-    price:{type:Number , required:true,default:0},
+const productSchema=mongoose.Schema({
+    name:{
+        type:String ,
+         required:true
+        },
+    description:{
+        type:String ,
+         required:true,
+        },
+    richDescription:{
+        type:String,
+        default:'richDescription'
+    },
+    image:{
+        type:String,
+        default:''
+    },
+    images:[{
+        type:String
+    }],
+    brand:{
+        type:String
+    },
+    price:{type:Number , default:0},
+    category:{
+        type:mongoose.Schema.Types.ObjectID,
+        ref:'Categories',
+        required:true
+    },
+    countInStock:{
+        type:Number,
+        min:0,
+        max:100,
+        required:true
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    isFeatured:{
+        type:Boolean,
+        default:false
+    },
+    numReviews:{
+        type:Number
+    },
+    dateCreated:{
+        type:Date,
+        default:Date.now,
+    }
 })
 
-module.exports = mongoose.model('Product',postSchema)
+productSchema.virtual('id').get(function(){
+    return this._id.toHexString()
+})
+productSchema.set('toJSON',{
+    virtual : true
+})
+
+module.exports = mongoose.model('Products',productSchema)
